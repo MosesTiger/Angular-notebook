b5d6897aa82e465cb75699e8c480e82cfe6f2752
Moses Tiger <150508386+MosesTiger@users.noreply.github.com>
2024-04-23 18:28:07 +0900

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,17 @@
+import 'zone.js';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { AddNoteComponent } from './add-note/add-note.component';
+import { NoteDetailComponent } from './note-detail/note-detail.component';
+
+const routes: Routes = [
+  { path: '', component: NotesListComponent },
+  { path: 'new', component: AddNoteComponent },
+  { path: 'note/:id', component: NoteDetailComponent },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+});
